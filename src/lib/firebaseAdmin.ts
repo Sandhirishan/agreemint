becc3d980987e23d1ece5d7cc6823b47548bef52
Sukh Oberoi <sukhoberoi24@gmail.com>
2024-10-20 00:33:48 +0530

    Update firebaseAdmin.ts

--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -1,14 +1,12 @@
 import admin from "firebase-admin";
 
-const serviceAccount = {
-    client_email: process.env.AUTH_FIREBASE_CLIENT_EMAIL,
-    private_key: process.env.AUTH_FIREBASE_PRIVATE_KEY,
-    project_id: process.env.AUTH_FIREBASE_PROJECT_ID,
-}
-
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+        clientEmail: process.env.AUTH_FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.AUTH_FIREBASE_PRIVATE_KEY,
+        projectId: process.env.AUTH_FIREBASE_PROJECT_ID,
+    }),
     databaseURL: process.env.FIREBASE_DATABASE_URL,
   });
 }
