5edf2783f7b48d01d584fa0dc4fed167f469780e
Sukh Oberoi <sukhoberoi24@gmail.com>
2024-10-20 10:44:13 +0530

    Update firebaseAdmin.ts

--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -4,8 +4,8 @@ if (!admin.apps.length) {
 	admin.initializeApp({
 		credential: admin.credential.cert({
 			clientEmail: process.env.AUTH_FIREBASE_CLIENT_EMAIL,
-			privateKey: process.env.FIREBASE_PRIVATE_KEY
-				? process.env.FIREBASE_PRIVATE_KEY.replace(/\n/gm, "\n")
+			privateKey: process.env.AUTH_FIREBASE_PRIVATE_KEY
+				? process.env.AUTH_FIREBASE_PRIVATE_KEY.replace(/\n/gm, "\n")
 				: undefined,
 			projectId: process.env.AUTH_FIREBASE_PROJECT_ID,
 		}),
